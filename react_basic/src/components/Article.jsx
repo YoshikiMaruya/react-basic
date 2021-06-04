7471b7f6a446d3734095f146b4b0bd3f2c5128eb
YoshikiMaruya <yoshimaru0514@gmail.com>
2021-06-04 16:46:50 +0900

    adjusted file

---
+++ react_basic/src/components/Article.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Article = () => {
+  return (
+    <>
+      <h2>Hello</h2>
+      <p>-------------------------</p>
+      <p>yoshikiです。</p>
+    </>
+  );
+};
+
+export default Article;
