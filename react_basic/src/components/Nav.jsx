56c7d516040c2f01d3caf24f0e00a36a8f0bffd6
YoshikiMaruya <yoshimaru0514@gmail.com>
2021-06-06 01:12:43 +0900

    changed the nav style

--- react_basic/src/components/Nav.jsx
+++ react_basic/src/components/Nav.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <nav style={{ background: "#666" }}>
       <ul style={{ display: 'flex', listStyle: 'none' }}>
-        <li style={{ margin: 10 }}><a href="/" style={{ color: "#fff", textDecoration: 'none' }}>Home</a></li>
-        <li style={{ margin: 10 }}><a href="/about" style={{ color: "#fff", textDecoration: 'none' }}>About</a></li>
+        <li style={{ margin: 10 }}><NavLink exact to="/" style={{ color: "#fff", textDecoration: 'none' }} activeStyle={{ color: '#aaf' }}>Home</NavLink></li>
+        <li style={{ margin: 10 }}><NavLink exact to="/about" style={{ color: "#fff", textDecoration: 'none' }} activeStyle={{ color: '#aaf' }}>About</NavLink></li>
       </ul>
     </nav>
   );
