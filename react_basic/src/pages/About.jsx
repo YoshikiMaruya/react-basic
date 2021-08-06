3768391dfd883616e5a22f320dc723123c76182d
YoshikiMaruya <yoshimaru0514@gmail.com>
2021-08-06 23:35:40 +0900

    changed about

--- react_basic/src/pages/About.jsx
+++ react_basic/src/pages/About.jsx
@@ -3,6 +3,7 @@ import Nav from '../components/Nav';
 import Header from '../components/Header';
 import AboutContent from '../components/AboutContent';
 import Footer from '../components/Footer';
+import Article from '../components/Article';
 
 const About = () => {
   return (
